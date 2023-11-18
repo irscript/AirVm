0c10799381cba83c96e500fed3421e6e9e604375
mindow <1794711263@qq.com>
2023-11-18 19:00:54 +0800

    文件格式修订2

--- airvm_runtime/airvm_bcfmt.h
+++ airvm_runtime/airvm_bcfmt.h
@@ -17,11 +17,11 @@ typedef struct
 
 //段表
 typedef struct{
-    uint32_t count;//项数量
+    uintptr_t count;//项数量
     struct{
-        uint32_t kind;//项类别
-        uint32_t offset;//项数据偏移
-    }item[];
+        uintptr_t kind;//项类别
+        uintptr_t offset;//项数据偏移，地址计算后是实际地址
+    }item[];//项
 }bcfmt_segtab;
 
 //表
@@ -32,7 +32,14 @@ typedef struct{
 bcfmt_typetab,//类型表
 bcfmt_functab;//函数表
 
-//本地函数表
-typedef struct{}bcfmt_nattab;
+typedef struct{
+    uintptr_t count;//项数量
+    struct{
+        uint32_t name;//动态库的名称，在符号表中的编号
+        uint32_t version;//编译时查询的版本号，用于兼容性判断
+        uintptr_t nat;//库加载验证后的插件表数据地址
+    }item[];//项
+}bcfmt_nattab,//主机函数表
+bcfmt_plugtab;//插件函数表
 
 #endif // __AIRVM_BCFMT_INC__
