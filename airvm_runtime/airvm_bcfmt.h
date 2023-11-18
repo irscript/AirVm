9d2493304e84cc7991ef85ae8e7e72de92b050c1
mindow <1794711263@qq.com>
2023-11-18 18:33:59 +0800

    文件格式定义

--- airvm_runtime/airvm_bcfmt.h
+++ airvm_runtime/airvm_bcfmt.h
@@ -15,6 +15,24 @@ typedef struct
 
 } bcfmt_header_t;
 
+//段表
+typedef struct{
+    uint32_t count;//项数量
+    struct{
+        uint32_t kind;//项类别
+        uint32_t offset;//项数据偏移
+    }item[];
+}bcfmt_segtab;
 
+//表
+typedef struct{
+    uintptr_t count;//项数量
+    uintptr_t item[];//项偏移，地址计算后是实际地址
+}bcfmt_strtab,//字符串表
+bcfmt_typetab,//类型表
+bcfmt_functab;//函数表
+
+//本地函数表
+typedef struct{}bcfmt_nattab;
 
 #endif // __AIRVM_BCFMT_INC__
